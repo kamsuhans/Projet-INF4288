f30fe15f6f8b2a239cd264c63b21cd1d1f59ee10
hans <hans.ngainsu@facsciences-uy1.cm>
2024-07-09 10:48:21 +0100

    new update done on Results

--- src/Pages/Results.jsx
+++ src/Pages/Results.jsx
@@ -7,12 +7,14 @@ const Results = () => {
   const { file, result } = location.state;
   const navigate = useNavigate();
   
+
   const handleBack = () => {
     navigate('/authenticate');
   };
 
   return (
-    <div className="container d-flex flex-column justify-content-center align-items-center min-vh-100 mb-7">
+
+    <div className="container d-flex flex-column justify-content-center align-items-center min-vh-100 mb-7 ">
       <h2 className="text-center mb-7">Authentication Results</h2>
       <div className="w-75">
         <h3>Results</h3>
@@ -31,7 +33,7 @@ const Results = () => {
           </div>
         </div>
         <div className="mb-3 text-center">
-          <h5 style={{ color: result.pourcentageDeSimilarite > 80 ? 'green' : 'red' }}>
+        <h5 style={{ color: result.pourcentageDeSimilarite >= 80 ? 'green' : 'red' }}>
             Matching Percentage: {result.pourcentageDeSimilarite}
           </h5>
         </div>
@@ -40,6 +42,38 @@ const Results = () => {
             Is Authentic: {result.estAuthentique ? 'Yes' : 'No'}
           </h5>
         </div>
+        <div className="mb-3 text-center">
+        <h5 className="card-title mt-4">Similarity</h5>
+          {result.diffence && (
+            <div className="card-text">
+            <h5>Province: {result.diffence.region}</h5>
+          <h5>Division: {result.diffence.departement}</h5>
+          <h5>Subdivision: {result.diffence.arrondissement}</h5>
+          <h5>No: {result.diffence.numeroActe}</h5>
+          <h5>Child name: {result.diffence.nomEnfant}</h5>
+          <h5>Born on: {result.diffence.dateNaissanceEnfant}</h5>
+          <h5>Born at: {result.diffence.lieuNaissanceEnfant}</h5>
+          <h5>sex: {result.diffence.sexe}</h5>
+          <h5>Fathers names: {result.diffence.nomPere}</h5>
+          <h5>Fathers birth place: {result.diffence.lieuNaissancePere}</h5>
+          <h5>Fathers Resident: {result.diffence.residencePere}</h5>
+          <h5>Fathers Professsion: {result.diffence.professionPere}</h5>
+          <h5>Mothers names: {result.diffence.nomMere}</h5>
+          <h5>Mothers birth place: {result.diffence.lieuNaissanceMere}</h5>
+          <h5>Mothers birth date: {result.diffence.dateNaissanceMere}</h5>
+          <h5>Mothers Resident: {result.diffence.residenceMere}</h5>
+          <h5>Mothers Professsion: {result.diffence.professionMere}</h5>
+          <h5>Drawn on the: {result.diffence.dresseLe}</h5>
+          <h5>In accordance with the declaration of: {result.diffence.surLaDeclarationDe1}</h5>
+          <h5> {result.diffence.surLaDeclarationDe2}</h5>
+          <h5>By us: {result.diffence.parNous1}</h5>
+          <h5> {result.diffence.parNous2}</h5>
+          <h5>Civil status Registrer for: {result.diffence.etatCivilCentreDe}</h5>
+          <h5>In the presence of: {result.diffence.assisteDe}</h5>
+          <h5>Total: {result.diffence.total}</h5>
+            </div>
+          )}
+        </div> 
         <div className="text-center mt-4">
           <button className="btn btn-secondary" onClick={handleBack}>Back to Authenticate</button>
         </div>
